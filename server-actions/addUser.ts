867988b2ae2fca2a10f3338abb215a4fe3b8c3de
Peacover <yer-raki@student.1337.ma>
2024-01-03 21:32:30 +0100

    confirmation email is working

--- server-actions/addUser.ts
+++ server-actions/addUser.ts
@@ -6,7 +6,6 @@ import bcrypt from "bcrypt";
 import { Resend } from "resend";
 import { render } from "@react-email/render";
 import EmailTemplate from "@/components/EmailTemplate/EmailTemplate";
-import sendEmail from "@/lib/utils/sendEmail";
 
 const addUser = async ({ name, email, role }: TAddUserInput) => {
   try {
@@ -20,12 +19,10 @@ const addUser = async ({ name, email, role }: TAddUserInput) => {
     }
     const generates_password = Math.random().toString(36).slice(-8);
     //send this password to the user email
-    // await sendEmail(email, generates_password);
     const resend = new Resend(process.env.RESEND_API_KEY);
     const ret_send_email = await resend.emails.send({
       from: "PanelAdmin <noreply@paneladmin.website>",
       to: email,
-      // to: "yousseferraki98@gmail.com",
       subject: "Welcome to PanelAdmin!",
       html: render(EmailTemplate({ name, email, password: generates_password })),
     });
