4ade0fd1dee061355f76b687c2bee4a2f4f186c8
Peacover <yer-raki@student.1337.ma>
2024-01-11 05:43:34 +0100

    styled sigIn page

--- components/AuthLayout/SignIn/SignInForm.tsx
+++ components/AuthLayout/SignIn/SignInForm.tsx
@@ -6,6 +6,7 @@ import { fetchLogin } from "@/server-actions/Login";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { useFormState } from "react-dom";
+import { toast } from "sonner";
 
 const handleSubmit = async (prevState: TState, formData: FormData) => {
   const email = formData.get("email");
@@ -17,6 +18,9 @@ const handleSubmit = async (prevState: TState, formData: FormData) => {
     });
 
     if (!user_login.success) {
+      user_login.error.issues.forEach((issue) => {
+        toast.error(issue.message);
+      });
       return {
         error: "Invalid Credentials!",
         success: false,
@@ -27,6 +31,7 @@ const handleSubmit = async (prevState: TState, formData: FormData) => {
       user_login.data.password
     );
     if (res.message !== "Login successfully") {
+      toast.error(res.message);
       return {
         error: "Invalid Credentials!!",
         success: false,
@@ -37,8 +42,9 @@ const handleSubmit = async (prevState: TState, formData: FormData) => {
       success: true,
     };
   } catch (error) {
+    toast.error((error as Error).message);
     return {
-      error: "Invalid Credentials test!!",
+      error: "Invalid Credentials!",
       success: false,
     };
   }
@@ -53,64 +59,56 @@ const SignInForm = () => {
 
   useEffect(() => {
     if (signInState.success) {
+      toast.success("Login successfully");
       push("/dashboard");
     }
   }, [signInState.success]);
 
   return (
-    <div>
-      <div className="rounded-lg border bg-card shadow-sm mx-auto max-w-sm bg-gray-100">
-        <form action={signInAction} className="flex flex-col space-y-4">
-          <div className="flex flex-col p-6 space-y-1">
-            <h3 className="tracking-tight text-2xl font-bold">Sign In</h3>
-            <p className="text-sm">
-              Enter your email and password to access your account
-            </p>
+    <div className="rounded-lg border bg-card shadow-sm mx-auto max-w-sm flex flex-col space-y-4">
+      <form action={signInAction}>
+        <div className="flex flex-col p-6 space-y-1">
+          <h3 className="tracking-tight text-2xl font-bold">Sign In</h3>
+          <p className="text-sm">
+            Enter your email and password to access your account
+          </p>
+        </div>
+        <div className="p-6 mb-3">
+          <div className="space-y-2">
+            <label className="text-sm font-medium " htmlFor="email">
+              Email
+            </label>
+            <input
+              className="flex h-10 rounded-md border px-3 py-2 text-sm bg-[#ffffff] w-full"
+              id="email"
+              name="email"
+              placeholder="john@example.com"
+              required
+              type="email"
+              autoComplete="email"
+            />
           </div>
-          <div className="p-6">
-            <div className="space-y-4 mb-0">
-              <div className="space-y-2">
-                <label className="text-sm font-medium " htmlFor="email">
-                  Email
-                </label>
-                <input
-                  className="flex h-10 rounded-md border px-3 py-2 text-sm bg-[#ffffff] w-full"
-                  id="email"
-                  name="email"
-                  placeholder="john@example.com"
-                  required
-                  type="email"
-                  autoComplete="email"
-                />
-              </div>
-              <div className="space-y-2">
-                <label className="text-sm font-medium " htmlFor="password">
-                  Password
-                </label>
-                <input
-                  className="flex h-10 rounded-md border px-3 py-2 text-sm bg-[#ffffff] w-full"
-                  id="password"
-                  name="password"
-                  required
-                  type="password"
-                  autoComplete="current-password"
-                />
-              </div>
-              <button
-                className="inline-flex items-center justify-center rounded-md text-sm font-medium h-10 px-4 py-2 w-full bg-gray-900 text-white"
-                type="submit"
-              >
-                Sign In
-              </button>
-            </div>
+          <div className="space-y-2 mt-4 mb-10">
+            <label className="text-sm font-medium " htmlFor="password">
+              Password
+            </label>
+            <input
+              className="flex h-10 rounded-md border px-3 py-2 text-sm bg-[#ffffff] w-full"
+              id="password"
+              name="password"
+              required
+              type="password"
+              autoComplete="current-password"
+            />
           </div>
-        </form>
-        {signInState.error && (
-          <div className="flex items-center justify-center p-6 border-t">
-            <p>{signInState.error}</p>
-          </div>
-        )}
-      </div>
+          <button
+            className="rounded-md text-sm font-medium h-10 w-full bg-gray-900 text-white"
+            type="submit"
+          >
+            Sign In
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
